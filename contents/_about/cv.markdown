682d5d9f78b7dc50c52310b8cd9d2c7afec05330
cedmax <marco.cedaro@gmail.com>
2014-10-19 20:42:51 +0100

    first commit

---
+++ contents/_about/cv.markdown
@@ -0,0 +1,23 @@
+I started studying theatre at the University of Bologna, but during the first year, attending the course “Creativity and Indipendence in Performing Arts” by Pontedera Foundation, I discovered that Bologna was not the center of the world.
+
+That’s why I left for Holstebro (DK): I wanted to get inspired by Odin Teatret. 
+After a stoge dinner, I dreamt about Hansel e Gretel’s witch house and I produced my first performance for children as Ursa Maior* (co-operating with Teatro Ridotto di Lavino di Mezzo - BO).
+
+I kept choosing my Masters and I ended up in Lithuania, where I observed Eimuntas Nekrošius’s work and wrote my degrees thesis: “L’Attualità della polvere: Cechov nel teatro di Nekrošius” (The actuality of dust: Cechov in Nekrošius’ theatre).
+
+Back from Lithuania, I produced my first two performance for adults.
+
+In 2007 I got pregnant - since no-one takes you seriously in Italy if you’re less than 40, it’s better to start a family soon - and I made two important choices: I quit being a mercenary actress to dedicate myself to my very projects and I ceased being the hard director of myself to become the dictatorial director of someone else.
+
+Hormonally hyperactive, I wrote a project for a startup and won a young enterprise funding contest made by the Bologna District. 
+When the original project (a design/communication office) was deflected to be a radical-chic shop by my colleagues, I realized it was time to stop and give birth to my first son.
+
+During the summer of 2011, while feeding my second son, the performance ‘Rosa’s Dream’ saw the light.
+Rosa’s Dream is what I consider my milestone: my bar was raised to a new standard.
+
+That’s why in the Autumn of 2011 ‘That Blue light’, a piece on the life of Madame Curie, got a lifting.
+In March 2012, postponed due to snow storm, ‘The snow queen-I don’t need you’ made its debut.
+
+Something more serious [here](http://uk.linkedin.com/in/irenegk)
+
+* [Ursa Maior](http://umteatro.it) projects have been supported in the years by the National Agency of the European Union, the Committee for Equal Opportunities of Padua University, Fondazione del Monte di Bologna e Ravenna, Coop Adriatica, Rosa Luxemburg Foundation of Berlino, Women Centre of Bologna, Bologna Bookfair for children.
---
+++ templates/js/script.js
@@ -0,0 +1,97 @@
+/*
+••••••••••••••••••••••••
+
+Powered by Type & Grids™
+www.typeandgrids.com
+
+••••••••••••••••••••••••
+*/
+
+jQuery.easing.def = "easeOutQuad";
+
+$(document).ready(function()
+{
+	
+	// For fluid video embedding
+	$(".video").fitVids();
+	// Project thumbnail hover
+	$(".projectThumbnail").on("mouseenter", function(e)
+	{
+		$(this).children(".projectThumbnailHover").fadeIn(300);
+		
+		$(this).children(".projectThumbnailHover").find("h4").css("display", "block");
+		$(this).children(".projectThumbnailHover").find("h4").css("opacity", "0");
+		$(this).children(".projectThumbnailHover").find("h4").delay(200).animate({left: '30', opacity: 1}, 200);
+		
+		$(this).children(".projectThumbnailHover").find("h5").css("display", "block");
+		$(this).children(".projectThumbnailHover").find("h5").css("opacity", "0");
+		$(this).children(".projectThumbnailHover").find("h5").delay(350).animate({left: '30', opacity: 1}, 200);
+	})
+	
+	$(".projectThumbnail").on("mouseleave", function(e)
+	{
+		$(this).children(".projectThumbnailHover").fadeOut(200);
+		$(this).children(".projectThumbnailHover").find("h4").animate({left: '0', opacity: 0}, 0);
+		$(this).children(".projectThumbnailHover").find("h5").animate({left: '0', opacity: 0}, 0);
+	})
+	
+	// Hide hover effect on touch devices
+	if (Modernizr.touch) {
+		$(".projectThumbnailHover").css("display", "none");
+		$(".projectThumbnailHover").css("visibility", "hidden");
+		$(".projectThumbnail").unbind("mouseenter");
+		$(".projectThumbnail").unbind("mouseleave");	
+	}	
+});
+
+// Remove site preloader after site is loaded
+$(window).load(function() {
+	$('#sitePreloader').delay(200).fadeOut(500, function() {
+		$(this).remove();
+	});
+	
+	// Fade site in
+	$(".container").delay(700).fadeIn(500);
+	$container = $("#creations").isotope({
+		animationEngine: 'jquery',
+		itemSelector : '.project',
+		layoutMode: "masonry",
+		animationOptions: {
+			duration: 500,
+			easing: 'easeOutQuad',
+			queue: false
+		}
+	});
+	$('#filters').on( 'click', 'a', function(e) {
+		e.preventDefault();
+		var filterValue = $(this).attr('data-filter');
+		$container.isotope({ filter: filterValue });
+	});
+});
+
+ $(document).ready(function () {
+    $("#visions").nanoGallery({
+      thumbnailWidth: 'auto',
+      thumbnailHeight: 250,
+      kind: 'flickr',
+   	  userID: '127244759@N04',
+      colorScheme: 'none',
+      locationHash: true,
+      viewer: 'fancybox',
+      viewerToolbar: {
+      	style: 'fullWidth',
+      	autoMinimize:15000
+      },
+      galleryToolbarHideIcons: true,
+      thumbnailHoverEffect: [{ name: 'labelAppear75', duration: 300 }],
+      theme: 'light',
+      thumbnailGutterWidth : 0,
+      thumbnailGutterHeight : 0,
+      thumbnailLabel: { hideIcons: true, display: true, position: 'overImageOnMiddle', align: 'center', displayDescription:false },
+      i18n: {
+      	breadcrumbHome: 'Visions'
+      }
+    });
+
+  });
+
